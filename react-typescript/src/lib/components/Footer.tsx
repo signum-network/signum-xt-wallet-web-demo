55ec02c1f281beb20fb7190c9877ed4117667bb8
ipr0310 <35440957+ipr0310@users.noreply.github.com>
2023-03-24 16:37:19 -0400

    Github Repo Link

--- react-typescript/src/lib/components/Footer.tsx
+++ react-typescript/src/lib/components/Footer.tsx
@@ -14,11 +14,6 @@ export const Footer = (): JSX.Element => (
     </a>
 
     <span> + </span>
-    <a href="https://reactjs.org/" target="_blank">
-      <img src="/react.svg" width="50" alt="SignumJS Logo" />
-    </a>
-
-    <span> + </span>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img
         src="/typescript.svg"
@@ -29,8 +24,21 @@ export const Footer = (): JSX.Element => (
     </a>
 
     <span> + </span>
+    <a href="https://reactjs.org/" target="_blank">
+      <img src="/react.svg" width="50" alt="SignumJS Logo" />
+    </a>
+
+    <span> + </span>
     <a href="https://vitejs.dev/" target="_blank">
       <img src="/vite.svg" width="50" alt="Powered by Signum Logo" />
+    </a>
+
+    <span> + </span>
+    <a
+      href="https://github.com/signum-network/signum-xt-wallet-web-demo"
+      target="_blank"
+    >
+      <img src="/github-mark.svg" width="50" alt="Github Repository" />
     </a>
   </footer>
 );
